fd9a2f301820b677d76f37f5b9235a1726303653
manizha <manizha.rasouly@gmail.com>
2022-04-26 11:24:42 +0300

    initial

---
+++ ESourcing.Products/Data/ProductContextSeed.cs
@@ -0,0 +1,58 @@
+﻿using ESourcing.Products.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESourcing.Products.Data
+{
+    public class ProductContextSeed
+    {
+        public static void DataSeed(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetConfihureProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetConfihureProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Name="Samsung",
+                    Summary="asdfadkfsadsfjkdsfjs",
+                    Description="adddddddddddddddfffffffffffffffffeeeee",
+                    ImageFile="test.png",
+                    Price=860.6M,
+                    Category="Smart Phone"
+
+                },
+                new Product()
+                    {
+                        Name="Iphone X",
+                        Summary="asdfadkfsadsfjkdsfjs",
+                        Description="adddddddddddddddfffffffffffffffffeeeee",
+                        ImageFile="test.png",
+                        Price=960.6M,
+                        Category="Smart Phone"
+
+                    },
+                new Product()
+                    {
+                        Name="Huawei Plus",
+                        Summary="asdfadkfsadsfjkdsfjs",
+                        Description="adddddddddddddddfffffffffffffffffeeeee",
+                        ImageFile="test.png",
+                        Price=960.6M,
+                        Category="Smart Phone"
+
+                    },
+            };
+        }
+    }
+}
---
+++ Esourcing.Sourcing/Data/SourcingContextSeed.cs
@@ -0,0 +1,79 @@
+﻿using Esourcing.Sourcing.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Esourcing.Sourcing.Data
+{
+    public class SourcingContextSeed
+    {
+       public static void SeedData(IMongoCollection<Auction> auctionCollection)
+        {
+            bool exist = auctionCollection.Find(p => true).Any();//if there is any item or data in collection
+            if (!exist)
+            {
+                auctionCollection.InsertManyAsync(GetPreconfigurations());
+            }
+        }
+
+        private static IEnumerable<Auction> GetPreconfigurations()
+        {
+            return new List<Auction>()
+            { 
+                new Auction()
+                {
+                    Name="Auction 1",
+                    Description="Auction desc 1",
+                    CreatedAt=DateTime.Now,
+                    StartedAt=DateTime.Now,
+                    FinishedAt=DateTime.Now.AddDays(10),
+                    ProductId="62593be92456586353a4b8d6",
+                    IncludedSellers=new List<string>()
+                    {
+                        "seler1@test.com",
+                        "seler2@test.com",
+                        "seler3@test.com"
+                    },
+                    Quantity="s",
+                    Status= (int)Status.Active
+                },
+                new Auction()
+                {
+                    Name="Auction 2",
+                    Description="Auction desc 2",
+                    CreatedAt=DateTime.Now,
+                    StartedAt=DateTime.Now,
+                    FinishedAt=DateTime.Now.AddDays(10),
+                    ProductId="62593be92456586353a4b8d6",
+                    IncludedSellers=new List<string>()
+                    {
+                        "seler1@test.com",
+                        "seler2@test.com",
+                        "seler3@test.com"
+                    },
+                    Quantity="s",
+                    Status= (int)Status.Active
+                },
+                new Auction()
+                {
+                    Name="Auction 3",
+                    Description="Auction desc 3",
+                    CreatedAt=DateTime.Now,
+                    StartedAt=DateTime.Now,
+                    FinishedAt=DateTime.Now.AddDays(10),
+                    ProductId="62593be92456586353a4b8d6",
+                    IncludedSellers=new List<string>()
+                    {
+                        "seler1@test.com",
+                        "seler2@test.com",
+                        "seler3@test.com"
+                    },
+                    Quantity="s",
+                    Status= (int)Status.Active
+                }
+            };
+        }
+    }
+}
